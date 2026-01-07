e648d5a2a736a6f5c819bdbf5a46470ad19af6e7
SaviNseveN7 <savinseven7@gmail.com>
2026-01-07 15:18:07 -0500

    start on gui and login

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI {
     public int screenWidth = 600;
@@ -8,6 +9,18 @@ public class GUI {
         JFrame frame = new JFrame();
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+
+        JButton loginButton = new JButton("Login");
+        mainPanel.add(loginButton);
+
+        JButton signUpButton = new JButton("Sign Up");
+        mainPanel.add(signUpButton);
+
+        JButton refreshButton = new JButton("Refresh");
+        mainPanel.add(refreshButton);
+
+        JButton newPostButton = new JButton("New Post");
+        mainPanel.add(newPostButton);
 
         frame.add(mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static GUI gui = new GUI();
     public static void main(String[] args) {
+    }
+
+    public static void signUp(String username, String password) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter("AccountData.txt"));
+        writer.write(username);
+        writer.write(" ");
+        writer.write(password);
+        writer.newLine();
+        writer.close();
     }
 }
