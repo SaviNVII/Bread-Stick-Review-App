67ab2fd1637f151e2bc747e0b6c0711ae7858bae
SaviN7 <savinseven7@gmail.com>
2026-01-20 13:18:05 -0500

    accoutn creatin g

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,9 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
     public static GUI gui = new GUI();
     public static Account account = new Account();
@@ -11,5 +17,23 @@ public class Main {
         String hashedPassword = Hasher.hashPassword(password);
         String jsonInputString = "{\"username\": \"" + username + "\", \"password\": \"" + hashedPassword + "\"}";
         System.out.println(jsonInputString);
+        try {
+            URL obj = new URI(url).toURL();
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            con.setRequestProperty("Content-Type", "application/json");
+
+            try (OutputStream os = con.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            int responseCode = con.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+            con.disconnect();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
